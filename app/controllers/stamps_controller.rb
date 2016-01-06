6abc29f043ee860e2f4bef03631eada6efc07f9b
harumukanon <mayizia8@gmail.com>
2016-01-06 13:20:48 +0000

    zoshoin joho toroku

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,4 +1,9 @@
 class StampsController < ApplicationController
+  
+  def show
+    @stamp = Stamp.find(params[:id])
+  end
+  
   def new
     @stamp = Stamp.new
   end
@@ -6,6 +11,8 @@ class StampsController < ApplicationController
   def create
     @stamp = Stamp.new(stamp_params)
     if @stamp.save
+      flash[:success] = "蔵書印情報が登録されました"
+      redirect_to @stamp
     else
       render 'new'
     end
