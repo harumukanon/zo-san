68b1c19a84e0cc882be4031262e0f08f1b8d9701
harumukanon <mayizia8@gmail.com>
2016-01-29 11:47:06 +0000

    holdings minus

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ActiveRecord::Base
   
     validates :title, presence: true, length: { maximum: 250 }
     validates :note, length: { maximum: 1000 }
-    validates :holding, presence: true, length: {maximum: 250 }
     
     attr_accessor :temp_stamp_id
 end
