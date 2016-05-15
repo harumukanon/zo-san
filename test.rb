2902e151aba6b5617c11a94d338903b8ae1c7c60
harumukanon <mayizia8@gmail.com>
2016-05-15 15:22:22 +0000

    hisashiburi

--- test.rb
+++ test.rb
@@ -1,7 +1,6 @@
 #-*- coding: utf-8 -*-
-require 'rubygems'
 require "sparql/client"
-
+  
 client = SPARQL::Client.new("http://id.ndl.go.jp/auth/ndla")
 results = client.query("
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
@@ -11,18 +10,13 @@ results = client.query("
     PREFIX xl: <http://www.w3.org/2008/05/skos-xl#>
     PREFIX ndl: <http://ndl.go.jp/dcndl/terms/>
     SELECT * WHERE {
-    ?uri1 foaf:primaryTopic ?uri2.
-    ?uri1 xl:prefLabel [ xl:literalForm ?heading; ndl:transcription ?yomi ].
-    OPTIONAL {
-    ?uri2 rda:dateOfBirth ?birth.
-    ?uri2 rda:dateOfDeath ?death.
-    }
-    FILTER (lang(?yomi) = 'ja-Kana').
-    FILTER regex(?heading, '^夏目').
-    }")
+        ?uri1 foaf:primaryTopic ?uri2.
+        ?uri1 xl:prefLabel [ xl:literalForm ?heading; ndl:transcription ?yomi ].
+        ?uri2 rda:dateOfBirth ?birth.
+        ?uri2 rda:dateOfDeath ?death.
+        }
+        ")
 
-    results.each do |solution|
-    puts "#{solution[:uri1]} | #{solution[:uri2]} | #{solution[:birth]} 
-    | #{solution[:death]} | #{solution[:heading]} | #{solution[:yomi]}"
-    end
-
+results.each do |solution|
+  puts "#{solution[:uri2]} | #{solution[:heading]}"
+end
